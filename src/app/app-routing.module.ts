9d6105350a380056b38ba7ad6374ced6fef1e980
Daniel Medani <danielgmedani@gmail.com>
2022-11-24 20:31:59 -0300

    clientes view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './components/pages/clientes/clientes.component';
+import { NovoClienteComponent } from './components/pages/novo-cliente/novo-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/novo', component: NovoClienteComponent },
+  { path: 'clientes/:id/pets', component: NovoClienteComponent },
+  { path: 'clientes/:id/pets/novo', component: NovoClienteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// components import
+import { AppComponent } from './app.component';
 import { ClientesComponent } from './components/pages/clientes/clientes.component';
 import { NovoClienteComponent } from './components/pages/novo-cliente/novo-cliente.component';
 import { PetsComponent } from './components/pages/pets/pets.component';
@@ -11,6 +13,17 @@ import { NovoPetComponent } from './components/pages/novo-pet/novo-pet.component
 import { LoginComponent } from './components/pages/login/login.component';
 import { UsuariosComponent } from './components/pages/usuarios/usuarios.component';
 import { NovoUsuarioComponent } from './components/pages/novo-usuario/novo-usuario.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+// angular material import
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -21,14 +34,23 @@ import { NovoUsuarioComponent } from './components/pages/novo-usuario/novo-usuar
     NovoPetComponent,
     LoginComponent,
     UsuariosComponent,
-    NovoUsuarioComponent
+    NovoUsuarioComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
