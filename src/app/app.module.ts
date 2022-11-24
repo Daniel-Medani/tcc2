a3fa411de1a01fd4a5953224bbff1e989fa7aa52
Daniel Medani <danielgmedani@gmail.com>
2022-11-24 18:32:01 -0300

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientesComponent } from './components/pages/clientes/clientes.component';
+import { NovoClienteComponent } from './components/pages/novo-cliente/novo-cliente.component';
+import { PetsComponent } from './components/pages/pets/pets.component';
+import { NovoPetComponent } from './components/pages/novo-pet/novo-pet.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { UsuariosComponent } from './components/pages/usuarios/usuarios.component';
+import { NovoUsuarioComponent } from './components/pages/novo-usuario/novo-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    NovoClienteComponent,
+    PetsComponent,
+    NovoPetComponent,
+    LoginComponent,
+    UsuariosComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
