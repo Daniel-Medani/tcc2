9e57752d73ec3bbf0a7b22e22b9c8aad3ad89fa0
Daniel Medani <danielgmedani@gmail.com>
2022-11-27 16:06:32 -0300

    pet-views completa

--- src/app/components/pages/pets/pets.component.ts
+++ src/app/components/pages/pets/pets.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Pet } from 'src/app/interfaces/pet';
+import { PetService } from 'src/app/services/pet.service';
 
 @Component({
   selector: 'app-pets',
   templateUrl: './pets.component.html',
-  styleUrls: ['./pets.component.scss']
+  styleUrls: ['./pets.component.scss'],
 })
-export class PetsComponent {
+export class PetsComponent implements OnInit {
+  constructor(private petService: PetService) {}
 
+  displayedColumns: string[] = [
+    'dono',
+    'nome',
+    'sexo',
+    'idade',
+    'raca',
+    'acoes',
+  ];
+  dataSource!: MatTableDataSource<Pet>;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnInit(): void {
+    this.getPets();
+  }
+
+  getPets(): void {
+    this.petService.getPets().subscribe({
+      next: (pet) => {
+        this.dataSource = new MatTableDataSource(pet);
+      },
+    });
+  }
 }
