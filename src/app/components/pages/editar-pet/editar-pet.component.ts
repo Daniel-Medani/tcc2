e7d9799b4c241be151c9ddfe20c836f16712853f
Daniel Medani <danielgmedani@gmail.com>
2022-11-27 12:42:58 -0300

    update-pets-funcionando

---
+++ src/app/components/pages/editar-pet/editar-pet.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Pet } from 'src/app/interfaces/pet';
+import { PetService } from 'src/app/services/pet.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-editar-pet',
+  templateUrl: './editar-pet.component.html',
+  styleUrls: ['./editar-pet.component.scss'],
+})
+export class EditarPetComponent implements OnInit {
+  constructor(
+    private petService: PetService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private location: Location
+  ) {}
+  pet!: Pet;
+  id!: any;
+
+  editPetForm = this.fb.group({
+    nome: ['', Validators.required],
+    sexo: ['', Validators.required],
+    idade: ['', Validators.required],
+    raca: ['', Validators.required],
+  });
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('idPet');
+    this.getPet();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  getPet(): void {
+    this.petService.getPet(this.id).subscribe((pet) => {
+      this.pet = pet;
+      this.editPetForm.controls['nome'].setValue(pet.nome);
+      this.editPetForm.controls['sexo'].setValue(pet.sexo);
+      this.editPetForm.controls['idade'].setValue(pet.idade);
+      this.editPetForm.controls['raca'].setValue(pet.raca);
+    });
+  }
+
+  updatePet(): void {
+    const { valid, value } = this.editPetForm;
+
+    if (valid) {
+      const pet: Pet = {
+        nome: value.nome!,
+        sexo: value.sexo!,
+        idade: value.idade!,
+        raca: value.raca!,
+      };
+
+      this.petService.patchPet(pet, this.id).subscribe(() => this.goBack());
+    }
+  }
+}
